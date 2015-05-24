7e506cc4496be4551e3a9cf2bd94850e3783fb1f
alexfts <alexandra.fetisova@gmail.com>
2015-05-24 11:39:21 -0700

    fixes bugs with pickle on the website

--- test_sending_json/hello.py
+++ test_sending_json/hello.py
@@ -4,11 +4,16 @@ from flask import request
 from flask import jsonify
 from pymongo import MongoClient
 app = Flask(__name__)
+#from sklearn.multiclass import OneVsRestClassifier
+#from sklearn.linear_model import LogisticRegression
+#from sklearn import svm
+#import numpy as np
 
 try:
     import cPickle as pickle
 except:
     import pickle
+    
 import helpers
 
 
@@ -32,17 +37,24 @@ def hello_world():
 
 @app.route('/result', methods=['GET', 'POST'])
 def result():
-    topic_tags = ["Eigenvalues_and_eigenvectors",
-              "Probability_density_function",
-              "Taylor_series",
-              "Substitution",
-              "Lagrange_interpolation"]
-    # Currently this simply reads the json, calculates the number of characters
-    # (to prove that the data is received) and returns this information.
-    # Here is where we would load the classifier, turn the content into a
-    # vector and predict the topic / find similar questions.
+    topic_tags = pickle.load(open("topics.bin", "r"))
     temp = request.get_json()
-    final_classifier = pickle.load(open("final.bin", "r"))
+    
+    # THE BELOW CODE WAS RUN IN RUN.PY TO CREATE final_classifier
+    #questions_raw = helpers.get_questions_with_topics(topic_tags)
+    #X = helpers.questions_to_X(questions_raw)
+    #y = helpers.questions_to_y(questions_raw, topic_tags)
+
+    #classifier = OneVsRestClassifier(svm.SVC(kernel='linear',
+     #                                    probability = True,
+      #                                   random_state=np.random.RandomState(0))
+ 
+       #                         )
+
+    #final_classifier = classifier.fit(X, y)
+    #!!!
+    
+    final_classifier = pickle.load(open("final.bin", "r"))  # - doesn't work
     
     statement = temp['statement']
     
